894835b2a4af3bb4c34d14e9f0f0ea4b1bb67325
Amal Francis <amalfra@gmail.com>
2017-06-28 00:28:49 +0530

    add linked list

---
+++ data structures/linked-list.c
@@ -0,0 +1,139 @@
+/*
+  Time Complexity:
+
+    Average case - access: O(n), search: O(n), insertion: O(1), deletion: O(1)
+    Worst case   - access: O(n), search: O(n), insertion: O(1), deletion: O(1)
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  struct node *next;
+  int data;
+};
+
+void display(struct node *head) {
+  struct node *temp = head;
+
+  if (temp == NULL) {
+    printf("List is empty!");
+  }
+
+  while (temp != NULL)
+  {
+    printf("%d ", temp->data);
+    temp = temp->next;
+  }
+}
+
+void add_front(struct node **head, int value) {
+  struct node *temp = malloc(sizeof(struct node));
+  temp->data = value;
+  temp->next = NULL;
+  if (*head != NULL) {
+    temp->next = *head;
+  }
+  *head = temp;
+}
+
+void add_end(struct node **head, int value) {
+  struct node *current = *head;
+  struct node *temp = malloc(sizeof(struct node));
+  temp->data = value;
+  temp->next = NULL;
+  if (current == NULL) {
+    *head = temp;
+  } else {
+    while (current->next != NULL)
+    {
+      current = current->next;
+    }
+    current->next = temp;
+  }
+}
+
+void remove_front(struct node **head) {
+  if (*head == NULL) {
+    printf("List is empty!");
+  } else {
+    struct node *temp = *head;
+    *head = (*head)->next;
+    printf("%d removed from list", temp->data);
+    free(temp);
+  }
+}
+
+void remove_end(struct node **head) {
+  if (*head == NULL) {
+    printf("List is empty!");
+  } else {
+    struct node *temp = NULL;
+    struct node *current = *head;
+    if ((*head)->next == NULL) {
+      temp = *head;
+      *head = NULL;
+    } else {
+      while (current->next != NULL)
+      {
+        temp = current;
+        current = current->next;
+      }
+      temp->next = NULL;
+    }
+    printf("%d removed from list", current->data);
+    free(current);
+  }
+}
+
+void main() {
+  int option = 0, value;
+  struct node *head = NULL, *temp;
+
+  do {
+    printf("\n\n");
+    printf("1. Display\n");
+    printf("2. Add to front\n");
+    printf("3. Add to end\n");
+    printf("4. Remove from front\n");
+    printf("5. Remove from end\n");
+    printf("6. Exit\n");
+    printf("Enter option: ");
+    scanf("%d", &option);
+    printf("\n");
+
+    switch(option) {
+      case 1:
+        display(head);
+        break;
+      case 2:
+        printf("Enter the value to add: ");
+        scanf("%d", &value);
+        add_front(&head, value);
+        break;
+      case 3:
+        printf("Enter the value to add: ");
+        scanf("%d", &value);
+        add_end(&head, value);
+        break;
+      case 4:
+        remove_front(&head);
+        break;
+      case 5:
+        remove_end(&head);
+        break;
+      case 6:
+        exit(0);
+        break;
+      default:
+        printf("Invalid option");
+        break;
+    }
+  } while(option > 0);
+
+  while (head != NULL)
+  {
+    temp = head;
+    head = head->next;
+    free(temp);
+  }
+}
